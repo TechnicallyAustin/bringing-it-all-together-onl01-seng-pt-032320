293c0a36941f53be6d857bb2104cad49d5601bb5
Technicallyaustin <Technicallyaustin@gmail.com>
2020-06-11 09:29:05 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,32 +58,37 @@ end
 
 def self.find_or_create_by(name:, breed:)
     sql = <<-SQL
-    SELECT * FROM dogs 
-    WHERE name = ?, breed = ?
-    SQL
-    
-   
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
 
-    if DB[:conn].execute(sql, name, breed).first
+
+      dog = DB[:conn].execute(sql, name, breed).first
+
+      if dog
         new_dog = self.new_from_db(dog)
-    else
+      else
         new_dog = self.create({:name => name, :breed => breed})
-    end
-    new_dog
-end
+      end
+      new_dog
+  end
+
 
 def self.find_by_name(name_to_find)
     sql = <<-SQL
     SELECT * FROM dogs WHERE name = ?
     SQL
-    DB[:conn].execute(sql, name_to_find)
+    DB[:conn].execute(sql, name_to_find).map do |row|
+        self.new_from_db(row)
+    end.first
+
 end
 
-def update(name, breed)
+def update
     sql = <<-SQL
     UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
-    DB[:conn].execute(sql, name, breed, self.id)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
 end
 end
     
